94d448b060ce151b718031674c7d0b95f5d4399f
sayan-sd <thegeek.sayan@gmail.com>
2024-05-16 16:16:53 +0530

    adding first draft

---
+++ scripts/form.js
@@ -0,0 +1,85 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+
+// ================= SHOW SCROLL UP =================
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+// previously selected theme by user 
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme)? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    //add or remove theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //save the current theme
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+
+
+// =========== CONTACT FORM ===============
+const inputs = document.querySelectorAll('.contact-input');
+
+inputs.forEach(ipt => {
+    ipt.addEventListener('focus', () => {
+        ipt.parentNode.classList.add('focus');
+        ipt.parentNode.classList.add('not-empty');
+    })
+    ipt.addEventListener('blur', () => {
+        if (ipt.value === '') {
+            ipt.parentNode.classList.remove('not-empty');
+        }
+        ipt.parentElement.classList.remove('focus');
+    })
+})
+
+
+
+// =================== COOKIE =================
+const cookieBox = document.querySelector('.cookie-wrapper');
+const cookieButtons = document.querySelectorAll('.cookie-btn');
+
+const showCookie = () => {
+    if (document.cookie.includes("At Designs")) return;
+    cookieBox.classList.add('show-cookie');
+
+    cookieButtons.forEach(cookieButton => {
+        cookieButton.addEventListener('click', () => {
+            cookieBox.classList.remove('show-cookie');
+
+            //if click accept btn
+            if (cookieButton.id == 'accept-cookie') {
+                //get cookies for 3 monts
+                document.cookie = "cookieBy = At Designs; max-age=" + 60*60*24*90;
+            }
+        })
+    })
+}
+
+window.addEventListener('load', showCookie);
---
+++ scripts/script.js
@@ -0,0 +1,115 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular-container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value-accordion-item');
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value-accordion-header');
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open');
+
+        toggleItem(item);
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem);
+        }
+    });
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value-accordion-content');
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style');
+        item.classList.remove('accordion-open');
+    }
+    else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px';
+        item.classList.add('accordion-open');
+    }
+}
+
+
+// ================= SHOW SCROLL UP =================
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+
+// =================== COOKIE =================
+const cookieBox = document.querySelector('.cookie-wrapper');
+const cookieButtons = document.querySelectorAll('.cookie-btn');
+
+const showCookie = () => {
+    if (document.cookie.includes("At Designs")) return;
+    cookieBox.classList.add('show-cookie');
+
+    cookieButtons.forEach(cookieButton => {
+        cookieButton.addEventListener('click', () => {
+            cookieBox.classList.remove('show-cookie');
+
+            //if click accept btn
+            if (cookieButton.id == 'accept-cookie') {
+                //get cookies for 3 monts
+                document.cookie = "cookieBy = At Designs; max-age=" + 60*60*24*90;
+            }
+        })
+    })
+}
+
+window.addEventListener('load', showCookie);
+
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+// previously selected theme by user 
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme)? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    //add or remove theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //save the current theme
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
